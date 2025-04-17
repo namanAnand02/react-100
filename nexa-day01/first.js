3841d367477968edcce76c8c047632c939bdd88f
predator27 <anand.naman997@gmail.com>
2025-04-17 19:23:30 +0530

    day-01- unordered list thingy added

--- nexa-day01/first.js
+++ nexa-day01/first.js
@@ -49,31 +49,35 @@ header2.style.fontSize = "15px"
 
 // ~~~~~~~~ REACT replica - using JS ~~~~~~~~~~~~~~~ //
 
-const React = {
-    createElement: function(tag, styles, text){
-        const newElement = document.createElement(tag)
+// const React = {
+//     createElement: function(tag, styles, text){
+//         const newElement = document.createElement(tag)
 
 
-        // styles = {fontSize:"50px", color:"red", backgroundColor:"pink"}
-        // so how do we add these to our new element at once 
-        // styles is an object- which holds diff style property
-        // to iterate on an object, we can use for-in loop 
+//         // styles = {fontSize:"50px", color:"red", backgroundColor:"pink"}
+//         // so how do we add these to our new element at once 
+//         // styles is an object- which holds diff style property
+//         // to iterate on an object, we can use for-in loop 
 
 
-        // newElement.style.fontSize = ...
-        // newElement.style.color = ....
-        for(let key in styles ){
-            newElement.style[key] = styles[key]
-            // newElement.fontSize = 30px
-            // newElement.style[key] is equivalent to newElement.style.key
+//         // newElement.style.fontSize = ...
+//         // newElement.style.color = ....
+//         for(let key in styles ){
+//             newElement.style[key] = styles[key]
+//             // newElement.fontSize = 30px
+//             // newElement.style[key] is equivalent to newElement.style.key
 
-        }
+//         }
+
+        
+
+//         newElement.innerText = text
 
 
-        newElement.innerText = text
-        return newElement 
-    }
-}
+
+//         return newElement 
+//     }
+// }
 
 // here React is an object. 
 // and createElement is its one of the property 
@@ -83,6 +87,92 @@ const React = {
 
 
 // say we also want to handle the appending of new element into the root by a function 
+
+// const ReactDOM = {
+//     render : function(element, root){
+//         // element is something we want to append into dom 
+//         // root is the div eleme with id = root in which we want to append this new element.
+//         root.append(element)
+//     }
+// }
+
+
+
+// // created new elements using React replica 
+// const header3 = React.createElement("h1", {}, "hello naman")
+// const header4 = React.createElement("h3", {}, "react- 101 studying")
+
+// // // const root = document.getElementById("root")
+// // root.append(header3)
+// // root.append(header4)
+
+
+
+// // or we could use ReactDOM object for that 
+
+// ReactDOM.render(header3, document.getElementById("root"))
+// ReactDOM.render(header4, document.getElementById("root"))
+
+
+// // // newElement with style added 
+// const header5 = React.createElement("h1", {fontSize:"50px", color:"red", backgroundColor:"pink"}, "hi from REACT")
+// ReactDOM.render(header5, document.getElementById("root"))
+
+// root.append(header5)
+
+
+
+// React - object 
+// ReactDOM - object 
+
+
+
+
+
+// ~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// How do we handle or create unordered list this way??
+
+// unordered list 
+// HTML 
+// CSS
+// JS 
+
+
+
+const React = {
+    createElement: function(tag, styles, text){
+        const newElement = document.createElement(tag)
+
+        // styles = {fontSize:"50px", color:"red", backgroundColor:"pink"}
+        // so how do we add these to our new element at once 
+        // styles is an object- which holds diff style property
+        // to iterate on an object, we can use for-in loop 
+
+        for(let key in styles ){
+            newElement.style[key] = styles[key]
+            // newElement.fontSize = 30px
+            // newElement.style[key] is equivalent to newElement.style.key
+
+        }
+
+        // add this extra thing so that we can develop things like undordered list where we'll need to send multiple texts 
+        if (typeof text === "object"){
+            // typeOf(arr) = object in js 
+            for (let val of text){
+                newElement.append(val)
+            }
+
+
+        }
+        else{
+            newElement.innerText = text
+        }
+
+
+        return newElement 
+    }
+}
 
 const ReactDOM = {
     render : function(element, root){
@@ -94,24 +184,14 @@ const ReactDOM = {
 
 
 
-// created new elements using React replica 
-const header3 = React.createElement("h1", {}, "hello naman")
-const header4 = React.createElement("h3", {}, "react- 101 studying")
-
-// // const root = document.getElementById("root")
-// root.append(header3)
-// root.append(header4)
 
 
 
-// or we could use ReactDOM object for that 
+const li1 = React.createElement("h1", {}, "HTMl")
+const li2 = React.createElement("h1", {}, "CSS")
+const li3 = React.createElement("h1", {}, "JS")
 
-ReactDOM.render(header3, document.getElementById("root"))
-ReactDOM.render(header4, document.getElementById("root"))
 
-
-// // newElement with style added 
-const header5 = React.createElement("h1", {fontSize:"50px", color:"red", backgroundColor:"pink"}, "hi from REACT")
-ReactDOM.render(header5, document.getElementById("root"))
-
-// root.append(header5)
+const unorderedList = React.createElement("ul",{fontSize:"10px", color:"red", backgroundColor:"pink"}, [li1,li2,li3] )
+// React accepts text as single, so we sent all three as an array - we'll extract them using loops 
+ReactDOM.render(unorderedList, document.getElementById("root"))
